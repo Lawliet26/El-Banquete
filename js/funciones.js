422e3ace7f15767baefb2f8b9eba0366dffb64ab
Daniel Esteban Ortega <48697770+Lawliet26@users.noreply.github.com>
2024-07-08 20:56:04 -0500

    Test: Jsbasics

--- js/funciones.js
+++ js/funciones.js
@@ -5,7 +5,7 @@
 let numero1;
 numero1 = 12;
 
-let numero2 = 15;
+let numero2 = 4;
 console.log(numero2);
 console.log(typeof numero2);
 
@@ -63,7 +63,138 @@ console.log(cadena.concat(" ","Luisa"," ","Fernanda")); //Concatena cadenas
 cadena = cadena + " Luisa" + " Fernanda";
 console.log(cadena); 
 
-numero1 = numero1.toString(); //Convierte un numero a cadena
-console.log(numero1);
+console.log(numero1.toString());//Convierte un numero a cadena
 
 console.log(numero1 + " Meses minimos"); //Concatenacion de cadena con numero
+
+// -------------------Operadores-------------------
+let total = numero1 + numero2;
+console.log(total);
+
+total = numero1 - numero2;
+console.log(total);
+
+total = numero1 * numero2;
+console.log(total);
+
+total = numero1 / numero2;
+console.log(total);
+
+total = numero1 % numero2;
+console.log(total);
+
+console.log(Math.pow(2,3)); //Potencia
+console.log(2**3); //Potencia
+
+console.log(Math.sqrt(81)); //Raiz cuadrada
+
+
+// -------------------Comparadores-------------------
+
+console.log(numero1 != numero2); //Diferente
+
+console.log(numero1 == numero2); //Igualdad
+
+console.log(numero1 > numero2); //Mayor
+
+console.log(numero1 >= numero2); //Mayor o igual
+
+console.log(numero1 < numero2); //Menor
+
+console.log(numero1 <= numero2); //Menor o igual
+
+console.log(numero1); 
+console.log(numero1 === "12" ); //Igualdad de valor y tipo
+console.log(numero1 == "12");  //Igualdad de valor
+
+
+// -------------------Funciones-------------------
+
+function sumar(num1,num2){
+    console.log(num1+num2); //La funcion imprime internamente el resultado
+    let respuesta = num1+num2; //No se puede acceder a esta variable fuera de la funcion
+}
+
+sumar(7,8);
+
+function restar(num1,num2){
+    return num1-num2; //Devuelve un valor que tiene que ser almacenado en una variable
+}
+
+let resultadores = restar(12,7); //Almacena el valor devuelto por la funcion
+console.log(resultadores);
+
+//-------------------Funciones predefinidas-------------------
+
+//alert("Hola mundo"); //Ventana emergente
+
+//let salida = confirm("¿Desea salir de la pagina?"); //Ventana emergente con confirmacion
+//alert(salida);
+
+/*let nume1 = prompt("Ingrese numero 1");
+let nume2 = prompt("Ingrese numero 2");
+
+console.log(parceFloat(nume1)+parceFloat(nume2));*/
+
+/*setTimeout(function(){
+    alert("Bienvenido a El Banquete");
+}, 1000); //Ejecuta una funcion despues de un tiempo */
+
+/*setInterval(function(){
+    console.log(new Date());
+}, 1000); //Ejecuta una funcion cada cierto tiempo */
+
+//-------------------Condicionales-------------------
+
+/*
+let edad = prompt("Ingrese su edad");
+
+if((edad > 0) && (edad < 10)){
+    alert("Eres menor de edad");
+}else if(edad == 10){
+    alert("Tienes 10 años");
+}
+else if((edad > 10) && (edad < 18)){
+    alert("Sigues siendo menor de edad");
+}else{
+    alert("Eres mayor de edad");
+}.
+*/
+/*
+switch (parseInt(edad)){
+    case 0:
+        alert("Eres un bebe");
+        break;
+    case 10:
+        alert("Eres menor de edad");
+        break;
+    case 18:
+        alert("Eres mayor de edad");
+        break;
+    case 30:
+        alert("Eres adulto");
+        break;
+    default:
+        alert("Esa es una edad?");
+}
+*/
+
+//-------------------Estructuras repetitivas-------------------
+
+let semana = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+
+for (let i = 0; i < semana.length; i++){
+    console.log(semana[i]);
+}
+
+for (let i = semana.length -1; i >=0 ; i--){
+    console.log(semana[i]);
+}
+
+console.log(semana.length);
+
+let nombre = "luisa";
+
+for (let i = 0; i < nombre.length ; i++){
+    console.log(nombre.charAt(i));
+}
